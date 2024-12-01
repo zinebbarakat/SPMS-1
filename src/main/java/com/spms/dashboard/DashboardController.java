9e7339c205f38055d832bac2a86ae1a89b7ed665
P4UL1N2 <22341451@live.uem.es>
2024-12-01 17:44:22 +0100

    dashboard but written in FX
    forget fxml

---
+++ src/main/java/com/spms/dashboard/DashboardController.java
@@ -0,0 +1,33 @@
+package com.spms.dashboard;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class DashboardController {
+
+    @FXML
+    void dashboard(MouseEvent event) {
+
+    }
+
+    @FXML
+    void lightMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settings(MouseEvent event) {
+
+    }
+
+    @FXML
+    void soilMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tempMenu(MouseEvent event) {
+
+    }
+
+}
